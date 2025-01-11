2b44733bb52c6c7f70c7014f6af27d2480ea4b5e
zahraaAed <zahraaalaaeddine94@gmail.com>
2025-01-11 20:34:29 +0200

    adding search button

--- weather-api-hack/src/Components/Weather.jsx
+++ weather-api-hack/src/Components/Weather.jsx
@@ -1,24 +1,47 @@
-import React from 'react';
+import React ,{useState}from 'react';
 import sunCloudImage from '../assets/Sun cloud angled rain.png';
 import rainImage from '../assets/noun-rain-2438520 1.png';
 import humidityImage from '../assets/noun-humidity-151847 1.png';
 import windImage from '../assets/noun-wind-4507827 1.png';
 import hourlyImage from '../assets/Group 650.png';
+import searchBar from '../assets/icon _Search.png';
 import './Weather.css';
 
 
-function Weather() {
+const Weather=()=>{
+  const [weatherData, setweatherData]=useState("")
+const [cityName, setcityName]=useState("")
+
   return (
     <div className="app">
       <header className="header">
         <h2>Lebanon</h2>
-        <input type="text" placeholder="Search by country" />
+        <div class="search">
+        <input type="text" placeholder="Search by country" class="search-input" />
+        <button className="search-icon">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className="icon"
+      >
+        <circle cx="11" cy="11" r="8" />
+        <line x1="21" y1="21" x2="16.65" y2="16.65" />
+      </svg>
+    </button>
+        </div>
       </header>
 
       <div className="current-weather">
         <div className="icon-section">
           <div className="weather-icon">
-            {/* Using the imported image for weather icon */}
+            {/* I changed this to button */}
             <img src={sunCloudImage} alt="weather icon" />
           </div>
           <div className="temperature">28°</div>
